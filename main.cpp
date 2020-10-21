bc79eda78459eddeb8b2f334bee73d429613dde2
yangyanmeng_i <yangyanmeng_i@didichuxing.com>
2020-10-21 16:14:02 +0800

    First Commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+int main(){
+	cout<<"hello yym!!!"<<endl;
+	vector<int>ve;
+	ve.push_back(12);
+	ve.push_back(155);
+	ve.push_back(17);
+	ve.push_back(15);
+	ve.push_back(14);
+	ve.push_back(13);
+	for(int i=0;i<5;i++){
+		cout<<ve[i]<<endl;
+	}
+	return 0;
+}
